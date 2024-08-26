ba299bbfa9cd31d6d84e2f9c9c9a74c0f2852c45
Ahrorbek Davronov <axrorbektatu@gmail.com>
2024-08-26 17:08:37 +0500

    new updaets

--- Program.cs
+++ Program.cs
@@ -1,40 +1,47 @@
 ﻿using System.Security.Principal;
+using Microsoft.VisualBasic;
 using OOPLESSON.CLasses;
 
 namespace KESCHA{
     class Program{
         static void Main(){
-            string userName = "";
-            Console.WriteLine("Ismingizni kiriting:");
-            userName = Console.ReadLine();
+
+            try{
+                string userName = "";
+                Console.WriteLine("Ismingizni kiriting:");
+                userName = Console.ReadLine();
+
+                Console.WriteLine("Yoshingizni kiriting:");
+                int userAge = Convert.ToInt32(Console.ReadLine());
+                
+                // Animal mushuk = new Animal(name: "Kescha", userAge: age, animalAge: 3);
+                Animal mushuk = new Cat(name: "Kescha", animalAge: 3);
+                Animal birdy = new Bird(name: "Kasee", animalAge: 5);
 
 
-            Console.WriteLine("Yoshingizni kiriting:");
-            int userAge = Convert.ToInt32(Console.ReadLine());
-
-            // Animal mushuk = new Animal(name: "Kescha", userAge: age, animalAge: 3);
-            Animal mushuk = new Cat(name: "Kescha", animalAge: 3);
-            Animal birdy = new Bird(name: "Kasee", animalAge: 5);
-
-
-            // mushuk.Age = 3;
-            mushuk.CalculateAgeDifference(userAge: userAge);
-            mushuk.Greeting(userName: userName);
-            mushuk.PrintInfo();
-            mushuk.CompareAges(userAge: userAge);
-            Console.WriteLine($"{mushuk.Name}ning do'stlari haqida aytib beraman!");
-            mushuk.TellAboutFriends(userName: userName, userAge: userAge);
+                // mushuk.Age = 3;
+                mushuk.CalculateAgeDifference(userAge: userAge);
+                mushuk.Greeting(userName: userName);
+                mushuk.PrintInfo();
+                mushuk.CompareAges(userAge: userAge);
+                Console.WriteLine($"{mushuk.Name}ning do'stlari haqida aytib beraman!");
+                mushuk.TellAboutFriends(userName: userName, userAge: userAge);
 
 
-            Console.WriteLine("\n");
+                Console.WriteLine("\n");
 
 
-            birdy.CalculateAgeDifference(userAge: userAge);
-            birdy.Greeting(userName: userName);
-            birdy.PrintInfo();
-            birdy.CompareAges(userAge: userAge);
-            Console.WriteLine($"{birdy.Name}ning do'stlari haqida aytib beraman!");
-            birdy.TellAboutFriends(userName: userName, userAge: userAge);
+                birdy.CalculateAgeDifference(userAge: userAge);
+                birdy.Greeting(userName: userName);
+                birdy.PrintInfo();
+                birdy.CompareAges(userAge: userAge);
+                Console.WriteLine($"{birdy.Name}ning do'stlari haqida aytib beraman!");
+                birdy.TellAboutFriends(userName: userName, userAge: userAge);
+            }catch(FormatException error){
+                Console.WriteLine($"{error.Message}");
+            }catch(Exception errors){
+                Console.WriteLine($"{errors.Message}");
+            }
         }
     }
 }
